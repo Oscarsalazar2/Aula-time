c3231b6c4c1ec579a93305a7c6f0b0630dd147c6
Oscarsalazar2 <107096615+Oscarsalazar2@users.noreply.github.com>
2024-12-17 20:56:47 -0600

    clases.sql

--- clases.sql
+++ clases.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 08-12-2024 a las 06:12:34
+-- Tiempo de generación: 18-12-2024 a las 03:56:19
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -37,7 +37,6 @@ CREATE TABLE `periodo` (
 --
 
 INSERT INTO `periodo` (`id`, `nombre`) VALUES
-(1, '08:00'),
 (2, '09:00'),
 (4, '10:00'),
 (6, '11:00'),
@@ -91,11 +90,6 @@ CREATE TABLE `reserva` (
 INSERT INTO `reserva` (`id`, `sala_id`, `periodo_id`, `dia`, `Profesor_alumno`, `materia`, `status`, `observacion`) VALUES
 (27, 1, 4, '2021-09-23', 'teste1', 'teste2', 1, 'teste3'),
 (28, 1, 4, '2021-09-30', 'teste1', 'teste2', 3, 'teste3'),
-(36, 1, 1, '2024-11-28', 'oscar gamaliel', 'bsd', 2, 'examen'),
-(37, 1, 1, '2024-12-05', 'oscar gamaliel', 'bsd', 2, 'examen'),
-(38, 1, 1, '2024-12-12', 'oscar gamaliel', 'bsd', 2, 'examen'),
-(39, 1, 1, '2024-12-19', 'oscar gamaliel', 'bsd', 2, 'examen'),
-(40, 1, 1, '2024-12-26', 'oscar gamaliel', 'bsd', 2, 'examen'),
 (42, 1, 6, '2024-12-05', 'oscar gamaliel', 'bsd', 2, 'examen'),
 (43, 1, 6, '2024-12-12', 'oscar gamaliel', 'bsd', 2, 'examen'),
 (44, 1, 6, '2024-12-19', 'oscar gamaliel', 'bsd', 2, 'examen'),
@@ -104,14 +98,18 @@ INSERT INTO `reserva` (`id`, `sala_id`, `periodo_id`, `dia`, `Profesor_alumno`, 
 (49, 2, 9, '2024-11-28', 'oscar gamaliel', 'km.nbnm', 1, 'prueba'),
 (50, 2, 6, '2024-11-28', 'sada', 'sadasd', 2, 'asdasd'),
 (51, 2, 2, '2024-11-28', 'sada', 'km.nbnm', 3, 'asdasd'),
-(53, 1, 1, '2024-12-03', 'Victor Loredo', 'Topicos', 1, 'Conexion a la base de datos'),
-(54, 1, 1, '2024-12-10', 'Victor Loredo', 'Topicos', 1, 'Conexion a la base de datos'),
-(55, 1, 1, '2024-12-17', 'Victor Loredo', 'Topicos', 1, 'Conexion a la base de datos'),
 (56, 1, 2, '2024-12-02', 'Victor Loredo', 'Topicos', 2, 'prueba'),
 (59, 2, 2, '2024-12-02', 'Victor Loredo', 'Uso general', 1, 'prueba'),
 (60, 1, 4, '2024-12-03', 'Victor Loredo', 'Topicos', 2, 'Conexion a la base de datos'),
 (61, 1, 7, '2024-12-03', 'Luis Gerardo', 'Prueba', 1, 'prueba'),
-(63, 2, 9, '2024-12-04', 'Victor Loredo', 'Topicos', 3, 'Conexion a la base de datos');
+(63, 2, 9, '2024-12-04', 'Victor Loredo', 'Topicos', 3, 'Conexion a la base de datos'),
+(66, 2, 9, '2024-12-17', 'asd', 'Uso general', 2, 'Conexion a la base de datos'),
+(67, 2, 9, '2024-12-24', 'asd', 'Uso general', 2, 'Conexion a la base de datos'),
+(70, 2, 9, '2024-12-20', 'asd', 'Uso general', 2, 'Conexion a la base de datos'),
+(71, 2, 9, '2024-12-27', 'asd', 'Uso general', 2, 'Conexion a la base de datos'),
+(74, 1, 4, '2024-12-12', 'Victor Loredo', 'Uso general', 2, 'prueba'),
+(76, 1, 8, '2024-12-12', 'Victor Loredo', 'Uso general', 2, 'Conexion a la base de datos'),
+(78, 2, 11, '2024-12-12', 'Luis Gerardo', 'Uso general', 1, 'prueba');
 
 --
 -- Disparadores `reserva`
@@ -142,12 +140,13 @@ CREATE TABLE `sala` (
 --
 
 INSERT INTO `sala` (`id`, `nombre`) VALUES
-(1, 'S 1'),
+(1, 'S 11'),
 (2, 'S 2'),
 (3, 'S 3'),
 (4, 'S 4'),
 (5, 'S 5'),
-(6, 'S 6');
+(12, 'S 6'),
+(13, 's 21');
 
 -- --------------------------------------------------------
 
@@ -172,7 +171,10 @@ INSERT INTO `trigger_periodo` (`id`, `accion`, `id_periodo`, `nombre_periodo`, `
 (1, 'DELETE', 14, '19:00', 'root@localhost', '2024-12-02 15:26:13'),
 (2, 'DELETE', 18, '', 'root@localhost', '2024-12-02 15:27:00'),
 (3, 'INSERT', 19, '23:00', 'root@localhost', '2024-12-02 17:12:18'),
-(4, 'DELETE', 19, '23:00', 'root@localhost', '2024-12-04 15:10:55');
+(4, 'DELETE', 19, '23:00', 'root@localhost', '2024-12-04 15:10:55'),
+(5, 'DELETE', 1, '08:00', 'admin@localhost', '2024-12-12 14:21:34'),
+(6, 'INSERT', 20, '8:00', 'admin@localhost', '2024-12-12 14:21:54'),
+(7, 'DELETE', 20, '8:00', 'admin@localhost', '2024-12-12 14:35:38');
 
 -- --------------------------------------------------------
 
@@ -187,18 +189,6 @@ CREATE TABLE `trigger_usuario` (
   `detalles` text DEFAULT NULL,
   `fecha` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
---
--- Volcado de datos para la tabla `trigger_usuario`
---
-
-INSERT INTO `trigger_usuario` (`id`, `operacion`, `usuario_id`, `detalles`, `fecha`) VALUES
-(1, 'DELETE', 10, 'Nombre: JUDITH, No Control: 22260020, Rol: user', '2024-12-04 00:28:00'),
-(2, 'DELETE', 3, 'Nombre: test, No Control: test, Rol: admin', '2024-12-04 00:28:05'),
-(3, 'INSERT', 12, 'Nombre: admin, No Control: admin, Rol: admin', '2024-12-04 06:30:41'),
-(4, 'INSERT', 13, 'Nombre: user, No Control: user, Rol: user', '2024-12-04 06:30:57'),
-(5, 'INSERT', 14, 'Nombre: asdasd, No Control: asdasd, Rol: user', '2024-12-08 03:46:59'),
-(6, 'DELETE', 14, 'Nombre: asdasd, No Control: asdasd, Rol: user', '2024-12-08 03:47:10');
 
 -- --------------------------------------------------------
 
@@ -219,12 +209,9 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`id`, `nombre`, `n_control`, `contraseña`, `rol`) VALUES
-(5, 'Oscar', '22260053', '827ccb0eea8a706c4c34a16891f84e7b', 'admin'),
-(8, 'teste', 'teste', '698dc19d489c4e4db73e28a713eab07b', 'admin'),
-(9, 'Fabiola', '12345', '81dc9bdb52d04dc20036dbd8313ed055', 'user'),
-(11, 'Ximena Amador Morante', '22260145', '827ccb0eea8a706c4c34a16891f84e7b', 'user'),
-(12, 'admin', 'admin', '21232f297a57a5a743894a0e4a801fc3', 'admin'),
-(13, 'user', 'user', 'ee11cbb19052e40b07aac0ca060c23ee', 'user');
+(1, 'teste', 'teste', '698dc19d489c4e4db73e28a713eab07b', 'admin'),
+(2, 'admin', 'admin', '21232f297a57a5a743894a0e4a801fc3', 'admin'),
+(3, 'user', 'user', 'ee11cbb19052e40b07aac0ca060c23ee', 'user');
 
 --
 -- Disparadores `usuario`
@@ -253,6 +240,32 @@ CREATE TRIGGER `after_insert_usuario` AFTER INSERT ON `usuario` FOR EACH ROW BEG
 END
 $$
 DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura Stand-in para la vista `vista_reservas`
+-- (Véase abajo para la vista actual)
+--
+CREATE TABLE `vista_reservas` (
+`reserva_id` int(11)
+,`dia` date
+,`Profesor_alumno` varchar(255)
+,`materia` varchar(255)
+,`status` int(11)
+,`observacion` text
+,`sala_nombre` varchar(255)
+,`periodo_hora` varchar(255)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura para la vista `vista_reservas`
+--
+DROP TABLE IF EXISTS `vista_reservas`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `vista_reservas`  AS SELECT `r`.`id` AS `reserva_id`, `r`.`dia` AS `dia`, `r`.`Profesor_alumno` AS `Profesor_alumno`, `r`.`materia` AS `materia`, `r`.`status` AS `status`, `r`.`observacion` AS `observacion`, `s`.`nombre` AS `sala_nombre`, `p`.`nombre` AS `periodo_hora` FROM ((`reserva` `r` join `sala` `s` on(`r`.`sala_id` = `s`.`id`)) join `periodo` `p` on(`r`.`periodo_id` = `p`.`id`)) ;
 
 --
 -- Índices para tablas volcadas
@@ -311,37 +324,37 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `periodo`
 --
 ALTER TABLE `periodo`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT de la tabla `reserva`
 --
 ALTER TABLE `reserva`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=64;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=79;
 
 --
 -- AUTO_INCREMENT de la tabla `sala`
 --
 ALTER TABLE `sala`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT de la tabla `trigger_periodo`
 --
 ALTER TABLE `trigger_periodo`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de la tabla `trigger_usuario`
 --
 ALTER TABLE `trigger_usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- Restricciones para tablas volcadas
